39c4831850b721ea97939122446eb994018c4491
Draden Boyer <dradenboyer@gmail.com>
2023-03-02 14:41:40 -0700

    added axios req

--- public/main.js
+++ public/main.js
@@ -2,7 +2,9 @@ let pikachu = document.querySelector('img')
 
 pikachu.addEventListener('click', (evt => {
     alert('grrrrrrrrrrrrrr')
-    
+    axios.get('http://localhost:4000/rollbar').then(response => {
+
+    }).catch(err => console.log(err))
 }))
 
 
--- server/server.js
+++ server/server.js
@@ -16,18 +16,24 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('public'))
-
+rollbar.info('site is visited')
 // console.log(__dirname + '../public/index.html')
 
 app.get('/', (req,res) => {
     res.status(200).sendFile(path.join(__dirname, '../public/index.html'))
-    rollbar.info('site is visited')
+
 })
 app.get('/css', (req,res) => {
     res.status(200).sendFile(path.join(__dirname, '../public/styles.css'))
 })
 app.get('/js', (req,res) => {
     res.status(200).sendFile(path.join(__dirname, '../public/main.js'))
+    
+})
+
+app.get('/rollbar', (req,res) => {
+    res.sendStatus(200)
+    rollbar.log('Pikachu was clicked!')
     
 })
 
