d8432be257f9d0e4d56797656d880f878de1c131
Draden Boyer <dradenboyer@gmail.com>
2023-03-02 14:52:15 -0700

    changed url

--- public/main.js
+++ public/main.js
@@ -2,7 +2,7 @@ let pikachu = document.querySelector('img')
 
 pikachu.addEventListener('click', (evt => {
     alert('grrrrrrrrrrrrrr')
-    axios.get('http://3.17.205.229//rollbar').then(response => {
+    axios.get('/rollbar').then(response => {
 
     }).catch(err => console.log(err))
 }))
